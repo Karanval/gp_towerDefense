89c8a1dad58e7bf0e3ada16b907e480d4875d5e7
Karolina Arancibiova <yotu.tuy@gmail.com>
2019-12-03 17:33:59 +0100

    basic spawning

--- TowerDefense.cpp
+++ TowerDefense.cpp
@@ -128,7 +128,7 @@ void TowerDefense::init() {
 	spawner = spawnObj->addComponent<SpawnController>();
 	spawner->setGameObjects(&gameObjects);
 	// TODO: replace with actual path when Grid is ready
-	spawner->startSpawningCycle({glm::vec2(5.0f,0.0f), glm::vec2(10.0f,0.0f) });
+	spawner->startSpawningCycle({glm::vec2(5.0f,0.0f), glm::vec2(7.0f,0.0f) });
 	gameObjects.push_back(spawnObj);
 
 	std::shared_ptr<GameObject> obj = createGameObject();
