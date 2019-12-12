8fe8e1fd62f2e1d633df4690aa26771465ed2ce9
Karolina Arancibiova <yotu.tuy@gmail.com>
2019-12-12 15:07:26 +0100

    Added main and end sounds

--- AudioManager.cpp
+++ AudioManager.cpp
@@ -1,26 +1,44 @@
 #include "AudioManager.hpp"
-#include <iostream>
 #include "SDL2-2.0.10\include\SDL.h"
+#include "SDL2_mixer-2.0.4\include\SDL_mixer.h"
+
+Mix_Music* music = NULL;
 
 AudioManager::AudioManager(GameObject* gameObject)
 	: Component(gameObject) {
-	int result = Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 512);
-	if (result < 0)
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
 	{
 		fprintf(stderr, "Unable to open audio: %s\n", SDL_GetError());
 		exit(-1);
 	}
+
 }
 
 
-void AudioManager::play(String filename) {
-	Mix_Chunk* high = Mix_LoadWAV(filename);
+void AudioManager::play(const char * filename) {
+	music = Mix_LoadMUS(filename);
+	if (music == NULL)
+		printf("Music was NOT loaded");
+	Mix_PlayMusic(music, -1);
 }
 
-void AudioManager::playOnce() {
-
+void AudioManager::playOnce(const char * filename) {
+	Mix_Chunk* sound = Mix_LoadWAV(filename);;
+	Mix_PlayChannel ( -1, sound, 0);
 }
 
 void AudioManager::playTimes(int amount) {
 
+}
+
+void AudioManager::cleanUP() {
+	Mix_FreeMusic(music);
+	Mix_CloseAudio();
+}
+
+void pause() {
+	if (Mix_PausedMusic() == 1)
+		Mix_ResumeMusic();
+	else
+		Mix_PauseMusic();
 }
